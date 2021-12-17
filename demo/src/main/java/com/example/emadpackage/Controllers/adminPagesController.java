4c9ba2df47dc12cac015acc52de86a784d36abba
emad-sfc <emad.sfc@gmail.com>
2021-12-17 22:49:47 +0530

    add page partially completed

--- demo/src/main/java/com/example/emadpackage/Controllers/adminPagesController.java
+++ demo/src/main/java/com/example/emadpackage/Controllers/adminPagesController.java
@@ -2,9 +2,11 @@ package com.example.emadpackage.Controllers;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.emadpackage.models.data.page;
@@ -31,5 +33,15 @@ public class adminPagesController {
 		model.addAttribute("pages",pages);
 		return "admin/pages/index";
 	}
-
+	
+	@GetMapping("/add")
+	public String add(@ModelAttribute page page) {    //Here we are passing object of our page entity
+//		model.addAttribute("page", new page());      //This method will also work, but we need to pass
+		return "admin/pages/add";						//Model object in paramaters
+	}
+	
+	
+	
+	
+	
 }
