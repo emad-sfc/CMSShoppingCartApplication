056669eba811b8d782aa29832e01fc8d249936ab
emad-sfc <emad.sfc@gmail.com>
2022-01-27 13:57:34 +0530

    Catergories menu added in home page

--- demo/src/main/java/com/example/emadpackage/Controllers/PagesController.java
+++ demo/src/main/java/com/example/emadpackage/Controllers/PagesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.emadpackage.models.data.page;
@@ -12,13 +13,26 @@ import com.example.emadpackage.models.data.pageRepository;
 @Controller
 @RequestMapping("/")
 public class PagesController {
-	
+
 	@Autowired
 	pageRepository pageRepo;
-	
+
 	@GetMapping
 	public String home(Model model) {
 		page page = pageRepo.findBySlug("home");
+		model.addAttribute("page", page);
+		return "page";
+	}
+
+	@GetMapping("/{slug}")
+	public String page(@PathVariable String slug,Model model) {
+		page page = pageRepo.findBySlug(slug);
+		
+		if(page==null)
+		{
+			return "redirect:/";
+		}
+		
 		model.addAttribute("page", page);
 		return "page";
 	}
--- demo/src/main/java/com/example/emadpackage/common.java
+++ demo/src/main/java/com/example/emadpackage/common.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.emadpackage.models.data.category;
+import com.example.emadpackage.models.data.categoryRepository;
 import com.example.emadpackage.models.data.page;
 import com.example.emadpackage.models.data.pageRepository;
 
@@ -16,10 +18,17 @@ public class common {
 	@Autowired
 	pageRepository pageRepo;
 	
+	@Autowired
+	categoryRepository categoryRepo;
+	
 	@ModelAttribute
-	public void sharedData(Model model) {      //in this method return type is void!
+	public void sharedData(Model model) {      //in this method return type is void! Therefore at the end of this method we are not returning any value.
 		List<page> pages = pageRepo.findAllByOrderBySortingAsc();
+		
+		List<category> categories = categoryRepo.findAll();
+		
 		model.addAttribute("cpages", pages);
+		model.addAttribute("ccategories", categories);
 	}
 
 }
